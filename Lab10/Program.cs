98eb082e9f2ea534b3d69457367851dc0772deb9
Minjo <alex.tito@tecsup.edu.pe>
2017-12-10 17:12:59 -0500

    Lab10

---
+++ Lab10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //origen de datos 
+            NorthwndDataContext context = new NorthwndDataContext();
+
+            //creación de consulta 
+            var product = (from p in context.Products
+                           where p.ProductID == 78
+                           select p).FirstOrDefault();
+
+            //Ejecución de consulta 
+            context.Products.DeleteOnSubmit(product);
+            context.SubmitChanges();
+        }
+    }
+}
